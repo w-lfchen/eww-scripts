832321759fcb00ad75fda64c7c753f68c132e90d
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-09-30 11:08:04 +0200

    First version of current window title bin

--- hyprland-current-window-title/src/main.rs
+++ hyprland-current-window-title/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use hyprland::data::Workspace;
+use hyprland::event_listener::EventListenerMutable as EventListener;
+use hyprland::shared::HyprDataActive;
+use hyprland::Result;
+
+fn main() -> Result<()> {
+    print_window_title();
+    let mut listener = EventListener::new();
+    listener.add_active_window_change_handler(|_, _| print_window_title());
+    listener.add_window_title_change_handler(|_, _| print_window_title());
+    listener.start_listener()
+}
+
+fn print_window_title() {
+    if let Ok(active_workspace) = Workspace::get_active() {
+        println!("{}", active_workspace.last_window_title)
+    } else {
+        println!("Couldn't get window title!")
+    }
 }
