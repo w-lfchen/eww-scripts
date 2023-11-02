a24a491d58bd6e7b19aa9044b48a7202d1685287
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2023-11-02 23:09:47 +0100

    Initial refactoring for multiple monitor support

--- hyprland-workspaces/src/main.rs
+++ hyprland-workspaces/src/main.rs
@@ -2,18 +2,35 @@ use hyprland::data::{Workspace, Workspaces};
 use hyprland::event_listener::EventListenerMutable as EventListener;
 use hyprland::shared::{HyprData, HyprDataActive};
 use hyprland::Result;
+use std::{env, process};
 
 fn main() -> Result<()> {
-    print_data();
+    let args: Vec<_> = env::args().collect();
+    match args.len() {
+        1 => listen_single(),
+        2 => {
+            // TODO: expect second argument to be the monitor in question, parse and handle accordingly
+            print_data_mult();
+            process::exit(0)
+        }
+        _ => {
+            println!("Wrong usage: too many arguments!");
+            process::exit(1)
+        }
+    }
+}
+
+fn listen_single() -> Result<()>{
+    print_data_single();
     let mut listener = EventListener::new();
-    listener.add_workspace_change_handler(|_, _| print_data());
-    listener.add_workspace_added_handler(|_, _| print_data());
-    listener.add_workspace_destroy_handler(|_, _| print_data());
-    listener.add_workspace_moved_handler(|_, _| print_data());
+    listener.add_workspace_change_handler(|_, _| print_data_single());
+    listener.add_workspace_added_handler(|_, _| print_data_single());
+    listener.add_workspace_destroy_handler(|_, _| print_data_single());
+    listener.add_workspace_moved_handler(|_, _| print_data_single());
     listener.start_listener()
 }
 
-fn print_data() {
+fn print_data_single() {
     let active_id: i32 = Workspace::get_active()
         .expect("Couldn't get active workspace!")
         .id;
@@ -38,4 +55,8 @@ fn print_data() {
     string.pop(); // remove last comma
     string.push(']');
     println!("{string}");
+}
+
+fn print_data_mult() {
+    // TODO
 }
