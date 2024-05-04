b3797491d42e0a41f375c5651052b59f1668a429
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-05-04 16:35:08 +0200

    feat(current-window-title): add monitor change handler

--- src/hyprland-current-window-title.rs
+++ src/hyprland-current-window-title.rs
@@ -1,15 +1,16 @@
 use hyprland::data::Workspace;
-use hyprland::event_listener::EventListenerMutable as EventListener;
+use hyprland::event_listener::EventListener;
 use hyprland::shared::HyprDataActive;
 use hyprland::Result;
 
 fn main() -> Result<()> {
     print_window_title();
     let mut listener = EventListener::new();
-    listener.add_active_window_change_handler(|_, _| print_window_title());
-    listener.add_window_title_change_handler(|_, _| print_window_title());
-    listener.add_window_close_handler(|_, _| print_window_title());
-    listener.add_workspace_change_handler(|_, _| print_window_title());
+    listener.add_active_monitor_change_handler(|_| print_window_title());
+    listener.add_active_window_change_handler(|_| print_window_title());
+    listener.add_window_close_handler(|_| print_window_title());
+    listener.add_window_title_change_handler(|_| print_window_title());
+    listener.add_workspace_change_handler(|_| print_window_title());
     listener.start_listener()
 }
 
